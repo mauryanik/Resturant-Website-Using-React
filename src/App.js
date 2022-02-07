035915d08e3092ff83cd83d5deaf5f3b3b683b70
Nikesh Maurya <nikeshinknit@gmail.com>
2022-02-07 21:51:48 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+// import Resturant  from './Component/Basics/Resturant'
+// import UseState from './Component/hooks/useState'
+//import UseEffect from './Component/hooks/UseEffect';
+import Todo from './Component/todoreact/todo';
+import Temp from './Component/weather/temp';
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Temp />
     </div>
-  );
-}
+  )
+};
 
-export default App;
+ //import reactDom from "react-dom"
+
+// // export const App = () => {
+// //   return (
+// //     <div>
+// //       Welcome to my Website.
+// //     </div>
+// //   )
+// // }
+
+// const App = () => {
+//   return (
+//     //React.fragment
+//     <>
+//       <MyName />
+//       <MyName />
+//       <MyName />
+//       <h1 className="myclassName">Hello React!!! {3 + 5} </h1>
+//       <p>react is good</p>
+//       <img src="" alt="" />
+//       <h4></h4>
+//     </>
+    
+//   );
+// };
+// const MyName = () => {
+//   return <h4>Nikesh Kumar Maurya </h4>
+// }
+
+//how react is actually looking into it
+// const App = () => {
+//   return React.createElement("h1", {}, "Hello React");
+// }
+export default App
